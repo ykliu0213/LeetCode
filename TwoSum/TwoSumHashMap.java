8b57a514a0a7dc4f0c09a132bf7470dece79f133
ykLiu <ken23912081@gmail.com>
2019-02-20 13:53:35 +0800

    remove surplus comment

--- TwoSum/TwoSumHashMap.java
+++ TwoSum/TwoSumHashMap.java
@@ -15,17 +15,3 @@ public class TwoSumHashMap {
 		throw new IllegalArgumentException("no two sum solution");
 	}
 }
-
-/*
-public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> numMap = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-        int content = target - nums[i];
-        if (numMap.containsKey(content)) {
-            return new int[] { numMap.get(content), i };
-        }
-        numMap.put(nums[i], i);
-    }
-    throw new IllegalArgumentException("No two sum solution");
-}
-*/
