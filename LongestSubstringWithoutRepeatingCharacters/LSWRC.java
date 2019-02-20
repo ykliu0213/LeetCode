578a2b9e9e866b1d0ae39b9613b54b0ef31a80b7
ykLiu <ken23912081@gmail.com>
2019-02-20 18:29:36 +0800

    misunderstanding , modify code

--- LongestSubstringWithoutRepeatingCharacters/LSWRC.java
+++ LongestSubstringWithoutRepeatingCharacters/LSWRC.java
@@ -3,14 +3,21 @@ package LongestSubstringWithoutRepeatingCharacters;
 public class LSWRC {
 	public int lengthOfLongestSubstring(String s) {
 		char[] b = new char[s.length()];
-		int count = 0;
+		int count = 0,ans = 0;
 		for(int i = 0 ; i < s.length() ; i ++) {
-			if(!contain(s.charAt(i), b)) {
-				count++;
-				b[i] = s.charAt(i);
-			}
+			for(int j = i ; j < s.length() ; j++){
+                if(!contain(s.charAt(j), b)){
+                    b[j] = s.charAt(j);
+                    count++;
+                }else{
+                    b = new char[s.length()];
+                    count = 0;
+                    break;
+                }
+                ans = Math.max(ans,count);
+            }
 		}
-		return count;
+		return ans;
     }
 	
 	public static boolean contain(char a , char[] b) {
--- LongestSubstringWithoutRepeatingCharacters/LSWRCHashSet.java
+++ LongestSubstringWithoutRepeatingCharacters/LSWRCHashSet.java
@@ -6,11 +6,13 @@ import java.util.Set;
 public class LSWRCHashSet {
 	public int lengthOfLongestSubstring(String s) {
 		Set<Character> subString = new HashSet<>();
-		int ans = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (!subString.contains(s.charAt(i))) {
-				subString.add(s.charAt(i));
-				ans++;
+		int ans = 0, i = 0, j = 0;
+		while (i < s.length() && j < s.length()) {
+			if (!subString.contains(s.charAt(j))) {
+				subString.add(s.charAt(j++));
+				ans = Math.max(ans, j - i);
+			} else {
+				subString.remove(s.charAt(i++));
 			}
 		}
 		return ans;
