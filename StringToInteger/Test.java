54d68ddd2fd7227d7ee247aedd7406822a303111
ykLiu <ken23912081@gmail.com>
2019-02-23 15:43:49 +0800

    add test part for StrToIntByChar

--- StringToInteger/Test.java
+++ StringToInteger/Test.java
@@ -2,12 +2,14 @@ package StringToInteger;
 
 public class Test {
 	public static void main(String[] args) {
-		String input = ".1";
+		String input = "42";
 		// also can try these following input :
 		//  "   -42" , "4193 with words" , "words and 987" , "-91283472332" , "20000000000000000000" , " " , "3.14159"
 		
 		StrToInt solution = new StrToInt();
+		StrToIntByChar solutionByChar = new StrToIntByChar();
 		System.out.println("Given input : \"" + input + "\"");
-		System.out.println("Implement with brute-force : " + solution.myAtoi(input));
+		System.out.println("Implement by string : " + solution.myAtoi(input));
+		System.out.println("Implement by char : " + solutionByChar.myAtoi(input));
 	}
 }
