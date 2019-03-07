3ae6e3deaed7f759dd99e762344e8af18e76c95d
ykLiu <ken23912081@gmail.com>
2019-03-08 00:19:50 +0800

    modify test file

--- BestTimeToBuyAndSellStocks/Test.java
+++ BestTimeToBuyAndSellStocks/Test.java
@@ -3,13 +3,13 @@ package BestTimeToBuyAndSellStocks;
 import java.util.Arrays;
 
 public class Test {
-public static void main(String[] args) {
-	int[] prices = {7,1,5,3,6,4};
-	// also can try these following input : 
-	// {7,6,4,3,1}
-	
-	BTBSS solution = new BTBSS();
-	System.out.println("Given input : " + Arrays.toString(prices));
-	System.out.println("Output : " + solution.maxProfit(prices));
-}
+	public static void main(String[] args) {
+		int[] prices = { 7, 1, 5, 3, 6, 4 };
+		// also can try these following input :
+		// {7,6,4,3,1}
+
+		BTBSS solution = new BTBSS();
+		System.out.println("Given input : " + Arrays.toString(prices));
+		System.out.println("Output : " + solution.maxProfit(prices));
+	}
 }
